34ca82c20d79d419a210ec8843235e1d081e074b
OM <30raiom@gmail.com>
2026-04-21 08:50:33 +0530

    Added UC1: Empty Tic-Tac-Toe Board

---
+++ src/main/java/org/example/emptytictactoe.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class emptytictactoe {
+    public static void main(String[] args) {
+        char[][] board = new char[3][3];
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                board[row][col] = '-';
+            }
+        }
+        System.out.println("Empty Tic-Tac-Toe Board:");
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                System.out.print(board[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
